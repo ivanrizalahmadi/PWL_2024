56d1d4a31f079be165162b91d08c3a3347f459dc
IvanRizal <ivanrizal36@gmail.com>
2025-03-06 11:25:25 +0700

    Praktikum ke 1

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,21 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/hello', function () {
     return 'Hello World';
 });
 Route::get('/world', function () {
     return 'World';
 });
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });
+Route::get('/posts/{post}/comments/{comment}', function
+    ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+    Route::get('/articles/{id}', function ($id) {
+        return 'Halaman Artikel dengan ID ' . $id;
+    });
+
+   
